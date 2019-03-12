52ae03230e567b6f57edd4d08200c12c212b42f4
Shunbo Cui <cuis13@mcmaster.ca>
2019-03-12 15:22:45 -0400

    updating average method

--- 2xb3_final_project/Group07_FinalProject/src/move_for_work/Average.java
+++ 2xb3_final_project/Group07_FinalProject/src/move_for_work/Average.java
@@ -13,8 +13,6 @@ import move_for_work.data.TwoTuple;
 public class Average {
     
 	public static ArrayList<TwoTuple> Average(ArrayList<JobInfo> jobs, int lo, int hi){
-		
-		
 		ArrayList<TwoTuple> List = new ArrayList<TwoTuple>();
 		
 		for (int i = 0; i < Province.values().length; i++) {
@@ -27,4 +25,29 @@ public class Average {
 		return List;
 	}
 	
+
+
+	public static void main(String[] args) throws Exception {
+		String industry = 
+				"Aboriginal public administration";
+				ArrayList<JobInfo> jobs = DatasetReader.readData("14100326.csv");
+		DatasetReader.cleanData(jobs);
+		JobsSort.sortBasicQuick(jobs);
+		
+		int left = JobFilter.getLeftIndex(jobs, 0, jobs.size(),
+				j -> j.industry.compareTo(industry));
+		int right = JobFilter.getRightIndex(jobs, 0, jobs.size(),
+				j -> j.industry.compareTo(industry));
+		/*
+		left = JobFilter.getLeftIndex(jobs, left, right,
+				j -> j.geography.compareTo(province));
+		right = JobFilter.getRightIndex(jobs, left, right,
+				j -> j.geography.compareTo(province));
+				*/
+		ArrayList<TwoTuple> list = Average(jobs, left, right);
+		for (int i = 0; i < list.size(); i++) {
+			System.out.println(list.get(i).first + " " + list.get(i).second);
+		}
+
+	}
 }
